7df6767793eedf408d97b613781c86ca611bcd5a
Apolinar Linares <41924872+apolinares@users.noreply.github.com>
2025-08-20 01:31:02 -0400

    proyecto listo para ser desplegado y evaluado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Autos } from './pages/cars/cars';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:Home},
+    {path: 'nosotros', component:About},
+    {path: 'automoviles', component:Autos},
+    {path: 'contacto', component:Contact},
+    {path: '**', redirectTo:''}
+];
