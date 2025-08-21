02ff8bb332c120a81e7220e9f07143dd446c63cb
Apolinar Linares <41924872+apolinares@users.noreply.github.com>
2025-08-21 01:19:03 -0400

    optimizaciones realizadas

--- src/app/services/cars.ts
+++ src/app/services/cars.ts
@@ -20,28 +20,28 @@ export class Cars {
       nombre: 'Deportivo Alto',
       descripcion : 'Ideal para ciudad',
       precio: 145500,
-      imagen: '/img/car-63930.jpg'
+      imagen: '/img/car-63930.webp'
     },
     {
       id: 2,
       nombre: 'Ferrari',
       descripcion: 'Potente y elegante',
       precio: 999200,
-      imagen: '/img/ferrari-627586.jpg'
+      imagen: '/img/ferrari-627586.webp'
     },
     {
       id: 3,
       nombre: 'Deportivo con puertas tipo mariposa',
       descripcion: 'Moderna, económica y Veloz',
       precio: 333900,
-      imagen: '/img/car-5954530.jpg'
+      imagen: '/img/car-5954530.webp'
     },
     {
       id: 4,
       nombre: 'Suzuki Convertible',
       descripcion: 'Moderna y muy económica',
       precio: 813900,
-      imagen: '/img/car-218113_1920.jpg'
+      imagen: '/img/car-218113_1920.webp'
     }
   ]
   constructor(){}
